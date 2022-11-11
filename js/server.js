22cd92c5252ea2d0baa9e599267008250d9bb20a
Developer Tuhin <116871268+TuhinNB@users.noreply.github.com>
2022-11-11 18:04:35 +0600

    Update server.js

--- js/server.js
+++ js/server.js
@@ -2,32 +2,3 @@
 <script nonce="29a6d0a5-b68a-44f3-a40b-70f76e966ff7">(function(w,d){!function(Z,_,ba,bb){Z.zarazData=Z.zarazData||{};Z.zarazData.executed=[];Z.zaraz={deferred:[],listeners:[]};Z.zaraz.q=[];Z.zaraz._f=function(bc){return function(){var bd=Array.prototype.slice.call(arguments);Z.zaraz.q.push({m:bc,a:bd})}};for(const be of["track","set","debug"])Z.zaraz[be]=Z.zaraz._f(be);Z.zaraz.init=()=>{var bf=_.getElementsByTagName(bb)[0],bg=_.createElement(bb),bh=_.getElementsByTagName("title")[0];bh&&(Z.zarazData.t=_.getElementsByTagName("title")[0].text);Z.zarazData.x=Math.random();Z.zarazData.w=Z.screen.width;Z.zarazData.h=Z.screen.height;Z.zarazData.j=Z.innerHeight;Z.zarazData.e=Z.innerWidth;Z.zarazData.l=Z.location.href;Z.zarazData.r=_.referrer;Z.zarazData.k=Z.screen.colorDepth;Z.zarazData.n=_.characterSet;Z.zarazData.o=(new Date).getTimezoneOffset();Z.zarazData.q=[];for(;Z.zaraz.q.length;){const bl=Z.zaraz.q.shift();Z.zarazData.q.push(bl)}bg.defer=!0;for(const bm of[localStorage,sessionStorage])Object.keys(bm||{}).filter((bo=>bo.startsWith("_zaraz_"))).forEach((bn=>{try{Z.zarazData["z_"+bn.slice(7)]=JSON.parse(bm.getItem(bn))}catch{Z.zarazData["z_"+bn.slice(7)]=bm.getItem(bn)}}));bg.referrerPolicy="origin";bg.src="/cdn-cgi/zaraz/s.js?z="+btoa(encodeURIComponent(JSON.stringify(Z.zarazData)));bf.parentNode.insertBefore(bg,bf)};["complete","interactive"].includes(_.readyState)?zaraz.init():Z.addEventListener("DOMContentLoaded",zaraz.init)}(w,d,0,"script");})(window,document);
 
 
-
-
-document.addEventListener("DOMContentLoaded",()=> {
-  const e = document.querySelector("video"),
-    n = e.getElementsByTagName("source")[0].src,
-    o = {};
-  if(Hls.isSupported()) {
-    var config = {
-      maxMaxBufferLength: 100,
-    };
-    const t = new Hls(config);
-    t.loadSource(n), t.on(Hls.Events.MANIFEST_PARSED, function(n, l) {
-      const s = t.levels.map(e => e.height);
-      o.quality = {
-        default: s[0],
-        options: s,
-        forced: !0,
-        onChange: e => (function(e) {
-          window.hls.levels.forEach((n, o) => {
-            n.height === e && (window.hls.currentLevel = o)
-          })
-        })(e)
-      };
-      new Plyr(e, o)
-    }), t.attachMedia(e), window.hls = t
-  } else {
-    new Plyr(e, o)
-  }
-});
