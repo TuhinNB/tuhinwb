bbcd3aa45b0e105dd11096bff1a8b6674de51c0e
Developer Tuhin <116871268+TuhinNB@users.noreply.github.com>
2022-11-22 00:40:19 +0600

    Create server-1.js

---
+++ js-server/server-1.js
@@ -0,0 +1,16 @@
+var playerInstance = jwplayer("myElement");
+playerInstance.setup({
+  playlist: [{
+    sources: [{
+      file: "https://live.hungama.com/linear/adipoli/playlist.m3u8",
+      type: "hls"
+    }],
+    image: "//content.jwplatform.com/thumbs/HQzagMgW-720.jpg",
+    title: "JW Player Support Demo",
+    description: "An example video embed."
+  }],
+  width: 640,
+  height: 360,
+  mute: false
+  
+});
